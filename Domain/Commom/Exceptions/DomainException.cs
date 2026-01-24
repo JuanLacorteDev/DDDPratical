6da09d1292ab023adf5ea9e2d8a5a82f5ecd54db
juan henrique <lacorte077@gmail.com>
2026-01-24 19:34:32 -0300

    Domain Exception base calss handler

---
+++ Domain/Commom/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDPratical.Domain.Commom.Exceptions;
+
+internal class DomainException : Exception
+{
+    public DomainException(string message) : base(message)
+    {
+    }
+
+    public static void When(bool hasError, string message)
+    {
+        if (hasError)
+            throw new DomainException(message);
+    }
+}
