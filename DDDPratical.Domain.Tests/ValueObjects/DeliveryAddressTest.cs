19da93216871d022c6e03255f9047f1e82f9674b
juan henrique <lacorte077@gmail.com>
2026-01-25 21:31:15 -0300

    Initial test setup

---
+++ DDDPratical.Domain.Tests/ValueObjects/DeliveryAddressTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDDPratical.Domain.Tests.ValueObjects;
+
+public class DeliveryAddressTest
+{
+    [Fact]
+    public void Create_Valid_Address()
+    {
+
+    }
+}
